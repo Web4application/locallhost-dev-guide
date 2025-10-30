ec433cfa42cb0361e68052ee5d3d34addc6f47ed
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-30 03:59:15 +0100

    Create Build.sh

---
+++ Build.sh
@@ -0,0 +1,5 @@
+Maven: mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8081
+Gradle: ./gradlew bootRun --args='--server.port=8081'
+npm scripts: "dev:8081": "PORT=8081 npm start"
+Docker build with port 8081
+CI/CD pipeline configuration
