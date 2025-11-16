e99f0ac7b81dc742cf6014db1f49602d6aa47eca
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-16 07:40:09 +0100

    Update and rename run.sh to .dockerfile/run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-$ docker run -d --name mlflow-server -p 5000:5000 \
-  -e MLFLOW_SERVER_HOST=0.0.0.0 \
-  -e MLFLOW_SERVER_PORT=5000 \
-  <kubuverse>/dhi-mlflow:<mlflow> server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000
